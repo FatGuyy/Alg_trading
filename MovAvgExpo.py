6a592d37529dbf3c5500f3edb199493cd5c83ec6
Kashyap Mavani <92817829+KashyapMavani@users.noreply.github.com>
2023-02-12 20:42:56 +0530

    Update MovAvgExpo.py

--- MovAvgExpo.py
+++ MovAvgExpo.py
@@ -10,7 +10,8 @@ def EWMA(data, ndays):
     EMA = pd.Series(data['Close'].ewm(span = ndays, min_periods = ndays - 1).mean(), 
                  name = 'EWMA_' + str(ndays)) 
     data = data.join(EMA)
-    print(data)
+    print(data) # this will give complete data with time stamp
+    print(data['EWMA_5']) # this will only give EWMA data with time stamp
     return data
 
 # Compute the 5-day EWMA
